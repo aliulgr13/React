7bc467be8ef9445f839ec240cb1db88596aba17b
Ali Ulgr <aliulger15@gmail.com>
2020-05-01 12:58:16 +0300

    old changes from mster

---
+++ week3/hackyourweather/build/precache-manifest.f75eafc1cdde978e73a6924f27f8989b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a5b44538a7c9db310e39247524857c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3695028437757e79439",
+    "url": "/static/css/main.cff4dc3a.chunk.css"
+  },
+  {
+    "revision": "0046d36e8a9c07eeebf1",
+    "url": "/static/js/2.c8e3ac6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8e3ac6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3695028437757e79439",
+    "url": "/static/js/main.404f44bb.chunk.js"
+  },
+  {
+    "revision": "1bb9ca93a391a832db1b",
+    "url": "/static/js/runtime-main.97981ac1.js"
+  }
+]);
